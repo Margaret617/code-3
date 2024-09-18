b3be661d8fb7ba875da851cba6ca3f133de25a1c
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-09-18 09:14:39 +0300

    modified init.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,21 @@
-if __name__ == "__main__":
-    conn = sqlite3.connect('your_database.db')
-    cursor = conn.cursor()
+import sqlite3
+
+connection = sqlite3.connect('Concerts.db')
+cursor = connection.cursor()
+
+# print(connection.total_changes)
+
+
+rows = cursor.execute("SELECT band_name FROM bands").fetchall()
+print(rows)
+
+rows = cursor.execute("SELECT venue_title FROM venues").fetchall()
+print(rows)
+
+rows = cursor.execute("SELECT venue_id FROM concerts").fetchall()
+print(rows)
+
+rows = cursor.execute("SELECT band_id FROM concerts").fetchall()
+print(rows)
+
+
